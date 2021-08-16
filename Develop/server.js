6c736a564df842fe652eb1c2a965b078dbf91a94
Shelby Butt <shelbybutt@Shelbys-MBP.hitronhub.home>
2021-08-16 00:16:56 -0400

    get routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 // file set up
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path');
 //port
 const PORT = 5000
 // app and body parser
@@ -8,6 +9,16 @@ let app = express();
 app.use(bodyParser.urlencoded({
 extended: true
 }));
+//get root route
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// get notes route
+app.get('/notes', function(req, res){
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
 // server check
 app.listen(5000, function()
 {console.log("NoteApp server is running at port 5000...")
